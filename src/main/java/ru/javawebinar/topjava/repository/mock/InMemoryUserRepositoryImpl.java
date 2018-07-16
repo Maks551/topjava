83ee961968f8bbec0293dafd2a71a73d123290ed
Maxim Kurkudyk <Maksik551@gmail.com>
2018-07-16 14:24:06 +0300

    Create InMemoryUserRepositoryImpl

---
+++ src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryUserRepositoryImpl.java
@@ -0,0 +1,67 @@
+package ru.javawebinar.topjava.repository.mock;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.*;
+import ru.javawebinar.topjava.repository.UserRepository;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+@Repository
+public class InMemoryUserRepositoryImpl implements UserRepository {
+    private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepositoryImpl.class);
+    private Map<Integer, User> repository = new ConcurrentHashMap<>();
+    private AtomicInteger counter = new AtomicInteger(0);
+
+    public static final int USER_ID = 1;
+    public static final int ADMIN_ID = 2;
+
+    {
+        save(new User(USER_ID, "User", "user@gmail.com", "password", Role.ROLE_USER));
+        save(new User(ADMIN_ID, "Admin", "admin@gmail.com", "admin", Role.ROLE_ADMIN));
+    }
+
+    @Override
+    public boolean delete(int id) {
+        log.info("delete {}", id);
+        if (!repository.containsKey(id)) return false;
+        repository.remove(id);
+        return true;
+    }
+
+    @Override
+    public User save(User user) {
+        log.info("save {}", user);
+        if (user.isNew()){
+            user.setId(counter.incrementAndGet());
+            repository.put(user.getId(), user);
+            return user;
+        }
+        return repository.computeIfPresent(user.getId(), (id, oldUser) -> user);
+    }
+
+    @Override
+    public User get(int id) {
+        log.info("get {}", id);
+        return repository.get(id);
+    }
+
+    @Override
+    public List<User> getAll() {
+        log.info("getAll");
+        return repository.values().stream()
+                .sorted(Comparator.comparing(AbstractNamedEntity::getName)
+                        .thenComparing(AbstractBaseEntity::getId))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public User getByEmail(String email) {
+        log.info("getByEmail {}", email);
+        return repository.values().stream().filter(user -> user.getEmail().equals(email)).findFirst().orElse(null);
+    }
+}
