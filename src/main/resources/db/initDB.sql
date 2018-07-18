3a0260c35e42ab80de6469c8f211c7f0dce13208
Maxim Kurkudyk <Maksik551@gmail.com>
2018-07-18 22:18:45 +0300

    Create Meals table

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,8 +1,19 @@
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS meals;
 DROP SEQUENCE IF EXISTS global_seq;
+DROP SEQUENCE IF EXISTS global_seq_meal;
 
 CREATE SEQUENCE global_seq START 100000;
+CREATE SEQUENCE global_seq_meal START 10000;
+
+CREATE TABLE meals
+(
+  id              INTEGER PRIMARY KEY DEFAULT nextval('global_seq_meal'),
+  dateTime        TIMESTAMP DEFAULT now()  NOT NULL,
+  description     VARCHAR                  NOT NULL,
+  calories        INTEGER                  NOT NULL
+);
 
 CREATE TABLE users
 (
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,6 +1,7 @@
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
+ALTER SEQUENCE global_seq_meal RESTART WITH 10000;
 
 INSERT INTO users (name, email, password) VALUES
   ('User', 'user@yandex.ru', 'password'),
@@ -9,3 +10,13 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (datetime ,description ,calories) VALUES
+  ('2018-05-30 10:00:00', 'Завтрак', 500),
+  ('2018-05-30 13:00:00', 'Обед', 1000),
+  ('2018-05-30 13:00:00', 'Ужин', 500),
+  ('2018-05-31 10:00:00', 'Завтрак', 500),
+  ('2018-05-31 13:00:00', 'Обед', 1000),
+  ('2018-05-31 13:00:00', 'Ужин', 510),
+  ('2018-06-01 14:00:00', 'Админ ланч', 510),
+  ('2018-06-01 21:00:00', 'Админ ужин', 1500);
