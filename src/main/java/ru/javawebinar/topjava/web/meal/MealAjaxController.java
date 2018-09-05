ee0f1e1e950d6cfc326d3a37f9b74744462c6c6f
Maxim Kurkudyk <Maksik551@gmail.com>
2018-09-05 14:46:10 +0300

    Binding

--- src/main/java/ru/javawebinar/topjava/web/meal/MealAjaxController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealAjaxController.java
@@ -1,15 +1,17 @@
 package ru.javawebinar.topjava.web.meal;
 
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.to.MealTo;
 import ru.javawebinar.topjava.to.MealWithExceed;
 
+import javax.validation.Valid;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
+
+import static ru.javawebinar.topjava.util.MealsUtil.createNewFromMealTo;
 
 @RestController
 @RequestMapping(value = "/ajax/profile/meals")
@@ -22,19 +24,23 @@ public class MealAjaxController extends AbstractMealController {
     }
 
     @Override
+    @GetMapping(value = "/{id}")
+    public Meal get(@PathVariable("id") int id) {
+        return super.get(id);
+    }
+
+    @Override
     @DeleteMapping(value = "/{id}")
     public void delete(@PathVariable("id") int id) {
         super.delete(id);
     }
 
     @PostMapping
-    public void createOrUpdate(@RequestParam("id") Integer id,
-                               @RequestParam("dateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateTime,
-                               @RequestParam("description") String description,
-                               @RequestParam("calories") int calories) {
-        Meal meal = new Meal(id, dateTime, description, calories);
-        if (meal.isNew()) {
-            super.create(meal);
+    public void createOrUpdate(@Valid MealTo mealTo) {
+        if (mealTo.isNew()) {
+            super.create(createNewFromMealTo(mealTo));
+        } else {
+            super.update(createNewFromMealTo(mealTo), mealTo.getId());
         }
     }
 
